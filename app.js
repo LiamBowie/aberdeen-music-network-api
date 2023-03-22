7f90f2169ff5c259fd634ce35f2b77de8365eaea
Liam Bowie <liambowie16@gmail.com>
2023-03-22 23:28:35 +0000

    Implement login and register functions with dummy db using passport js

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const db = require('./db');
 const PORT = process.env.PORT || 4001;
+const app = express();
 
+// Middleware
+app.use(cors({
+    origin: 'http://localhost:5173', // <-- location of the host site we're connecting to
+    credentials: true
+}));
+app.use(
+    session({
+      secret: "f4z4gslG6g", // This will eventually be stored as an ENV variable
+      cookie: { maxAge: 172800000, secure: false },
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+app.use(morgan('short'));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+require('./server/passport')(passport);
+
+// Routes
+app.post('/login', (req, res) => {
+    console.log(req.body)
+});
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    const newUser = await db.createUser({username, password});
+    if(newUser) {
+        res.status(201).json({
+            msg: `User (${username}) successfully created!`,
+            newUser
+        })
+    } else { 
+        res.status(500).json({
+          msg: "Something went wrong"
+        });
+    }
+    console.log(db.records);
+});
+
+app.get("/user", (req, res) => {
+    console.log(req.body)
+});
+
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-})
+});
---
+++ server/passport.js
@@ -0,0 +1,27 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+module.exports = passport => {
+    
+    passport.use(new LocalStrategy(
+        function(username, password, done){
+            db.findByUsername(username, (err, user) => {
+            if(err) return done(err);
+            if(!user) return done(null, false);
+            if(user.password != password) return done(null, false);
+            return done(null, user);
+            });
+        })
+    );
+    
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    
+    passport.deserializeUser((id, done) => {
+        db.findById(id, function (err, user) {
+            if (err) return done(err); 
+            done(null, user);
+        });
+    });
+
+}
